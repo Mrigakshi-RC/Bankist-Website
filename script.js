43f291b1f54f5890b45098f915d48fb7da7dca17
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-09-02 19:10:12 +0530

    feat: implement scrolls

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
-  e.preventDefault()
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -19,7 +21,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn=>btn.addEventListener('click', openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -27,5 +29,29 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+btnScrollTo.addEventListener('click', function () {
+  // const s1coords=section1.getBoundingClientReact(); //coordinates of section relative to the visible viewport, width, etc
+  // window.scrollTo({
+  //   left: s1coords.left+window.pageXOffset, //pageOffsets make sure these positions are not relative to the viewport
+  //   top: s1coords.top+window.pageYOffset,
+  //   behavior: "smooth"
+  // })
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// document.querySelectorAll('.nav__link').forEach(function(el){
+//   e.preventDefault();
+//   const id = this.getAttribute('href')
+//   document.querySelector(id).scrollIntoView({behavior:'smooth'}) //not efficient for 10000 elements, then 1000 copies of function
+// })
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href'); //e.target is the element that was clicked
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
